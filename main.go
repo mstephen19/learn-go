122697b628c3132ea83ab603faf47f0c3a52f92a
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2023-01-03 02:27:33 +0100

    basic HTTP server

--- main.go
+++ main.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
+	"net/http"
 )
 
-func wait(channel *chan string) {
-	// Wait for a message to be received on the channel
-	msg := <-*channel
-	fmt.Println("received", msg)
-	// Sleep for 2 seconds
-	time.Sleep(2 * time.Second)
-	fmt.Println("slept")
-	// Send a message onto the channel
-	*channel <- "b"
-	fmt.Println("sent message")
-}
-
 func main() {
-	var channel = make(chan string)
-	var wg sync.WaitGroup
-
-	go wait(&channel)
-
-	// Send a message on the channel letting it know that it can start
-	channel <- "a"
-
-	// Wait for a message on the channel
-	fmt.Println(<-channel)
-
-	wg.Add(1)
-	go func() {
-		fmt.Println(<-channel)
-		wg.Done()
-	}()
-
-	close(channel)
-	wg.Wait()
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"foo":"bar"}`))
+		w.Header().Set("content-type", "application/json")
+	}))
+	http.ListenAndServe(":8000", nil)
 }
