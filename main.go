2a9de5196f8036eb57d6d255505fd12e0ed4a5e4
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2023-01-08 21:53:45 +0100

    more experimentation with interface embedding

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"bytes"
+	"io"
+)
+
+type Happy interface {
+	io.Reader
+	foo() string
+}
+
+type happy struct {
+	*bytes.Reader
+	name string
+}
+
+func (h *happy) foo() string {
+	return ""
+}
+
 func main() {
+	var x Happy = &happy{}
 }
