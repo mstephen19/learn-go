fc34388025c8b3dc8e60c52555c91f9ca0ae8021
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2023-01-10 02:53:46 +0100

    slices n copying

--- main.go
+++ main.go
@@ -1,68 +1,10 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-	"sync"
-)
-
-type Happy interface {
-	io.Reader
-	foo() string
-}
-
-type happy struct {
-	*bytes.Reader
-	name string
-}
-
-func (h *happy) foo() string {
-	return ""
-}
-
-type A interface {
-	foo()
-}
-
-type C struct {
-	*B
-}
-
-type B struct {
-	Name int
-}
-
-func (b B) foo() {
-	fmt.Println("ran")
-}
-
-type Modifier interface {
-	Modify()
-}
-
-type L struct {
-	lock *sync.Mutex
-	name string
-}
-
-func (l *L) Modify() {
-	l.name = "foo"
-}
-
-type M struct {
-	lock *sync.Mutex
-	name string
-}
-
-func (m *M) Modify() {
-	m.name = "foo"
-}
-
-type S interface{}
+import "fmt"
 
 func main() {
-	var B S = "foo"
-
-	x := B.(string)
+	array := [20]byte{}
+	slice := array[:10]
+	copy(slice, []byte("foo barrrr"[:3]))
+	fmt.Println(array)
 }
