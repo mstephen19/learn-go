43591372a6e5dac4bf0faaeb284dc89aa4ff1072
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2023-01-10 03:07:51 +0100

    advanced slices

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import "fmt"
 
+func copyStuff(s []byte) {
+	// Since our slice has a length of zero,
+	// the byte 97 is added to the beginning
+	// of the slice's underlying array.
+	// But since we are reassigning it, we create
+	// a brand new slice. However, this brand
+	// new slice still has the same underlying array.
+	s = append(s, 'a')
+}
+
 func main() {
 	array := [20]byte{}
-	slice := array[:10]
-	copy(slice, []byte("foo barrrr"[:3]))
+	slice := array[:0]
+	copyStuff(slice)
 	fmt.Println(array)
+	fmt.Println(slice)
 }
